6ff7ea26cf4ceee1abab8920688723c5e01eeca6
YukiFujimine <study.yuki0115@gmail.com>
2020-02-06 12:26:42 +0900

    北ソフトQ7_3

---
+++ Q7_3.java
@@ -0,0 +1,12 @@
+public class Q7_3{
+	public static void main(String[] args){
+		Dog dog1=new Dog();
+		Dog dog2=new Dog();
+		dog1.SetName("レオ");
+		dog1.SetAge(3);
+		dog2.SetName("サンディー");
+		dog2.SetAge(8);
+		dog1.ShowProfile();
+		dog2.ShowProfile();
+	}
+}
