c4b0787b1eeb19b6f5daa25cbc4966e5cceb10f3
YukiFujimine <59949394+YukiFujimine@users.noreply.github.com>
2020-02-10 15:15:02 +0900

    北ソフトQ7_5-6

--- CoinCase.java
+++ CoinCase.java
@@ -1,16 +1,16 @@
 public class CoinCase{
 	String Type;
 
-	int coiniC500=0;
+	int coinC500=0;
 	int coinC100=0;
 	int coinC50=0;
 	int coinC10=0;
 	int coinC5=0;
 	int coinC1=0;
 
-	public CoinCase(){
+	CoinCase(){
 	}
-	public void AddCoins(int type,int count){
+	void AddCoins(int type,int count){
 		switch(type){
 			case 500:
 				coinC500+=count;
@@ -31,30 +31,26 @@ public class CoinCase{
 				coinC1+=count;
 				break;
 		}
+		}
 
-		public int GetCount(int type){
-			switch(type){
+	 int GetCount(int type){
+		switch(type){
 			case 500:
 				return coinC500;
-				break;
 			case 100:
 				return coinC100;
-				break;
 			case 50:
 				return coinC50;
-				break;
 			case 10:
 				return coinC10;
-				break;
 			case 5:
 				return coinC5;
-				break;
 			case 1:
 				return coinC1;
-				break;
 			}
+		return 0;
 		}
-		public int GetAmount();
+		public int GetAmount(){
 			return(500*coinC500+100*coinC100+50*coinC50+10*coinC10+5*coinC5+1*coinC1);
 	}
 }
